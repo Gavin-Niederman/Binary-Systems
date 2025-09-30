0493c5fdc8284ccfea8daee437c670738c87b97a
Gavin Niederman <gavinniederman@gmail.com>
2025-09-30 09:52:57 -0700

    feat: more movement work! (doesnt actually work)

--- player.gd
+++ player.gd
@@ -6,19 +6,39 @@ extends Node3D
 var velocity: Vector3
 var view_vector: Vector3 = Vector3(1.0, 0, 0)
 
+var movement_vector: Vector2 = Vector2(0, 0)
+
 @export var movement_speed: float = 10;
 @export var ground_acceleration: float = 0.5;
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	var new_movement_vec = Vector2(
+		Input.get_action_strength("left") - Input.get_action_strength("right"),
+		Input.get_action_strength("forward") - Input.get_action_strength("back")
+	)
+	movement_vector = new_movement_vec.normalized()
+	
+	if event is InputEventMouseMotion:
+		var delta_x = event.relative.x
+		var delta_y = event.relative.y
+		
+		var rot = quaternion.get_euler()
+		rot.y += delta_x / 1000
+		quaternion = Quaternion.from_euler(rot)
+
+func vec2_to_vec3(vec2: Vector2) -> Vector3:
+	return Vector3(vec2.x, 0, vec2.y)
+
 func _physics_process(delta: float) -> void:
-	var desired_movement = Vector3(view_vector.x, 0, view_vector.z).normalized()
+	var desired_movement = vec2_to_vec3(movement_vector.rotated(quaternion.get_euler().y))
 	var horizontal_velocity = Vector3(view_vector.x, 0, view_vector.z)
 	
 	# no grounded check yet
-	var applied_acceleration = min(ground_acceleration, movement_speed / horizontal_velocity.length())
-	var new_velocity = horizontal_velocity.length() + applied_acceleration
-	desired_movement += desired_movement * (Vector3(new_velocity, new_velocity, new_velocity))
+	var movement = velocity.lerp(desired_movement * movement_speed, ground_acceleration)
 
-	var movement = desired_movement
 	position += movement
 	
 	
